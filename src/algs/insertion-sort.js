eea1e1341cf5c5cd16f5ffbb656e8cbd76c4b3ae
Peter Szerzo <peterszerzo@users.noreply.github.com>
2017-03-26 08:23:58 +0200

    Update insertion-sort.js

--- src/algs/insertion-sort.js
+++ src/algs/insertion-sort.js
@@ -9,38 +9,4 @@ function insertionSort (arr) {
   }
 }
 
-function insertionSortAsync (arr, next) {
-  const dummyTimestep = 0;
-  function innerLoop (j, i, temp, next) {
-    if (!(j > 0 && arr[j - 1] > temp)) {
-      return next()
-    }
-    setTimeout(() => {
-      swap(arr, j, j - 1)
-      innerLoop(j - 1, i, temp, next)
-    }, dummyTimestep)
-  }
-
-  function outerLoop (i, next) {
-    if (i === arr.length) {
-      return next()
-    }
-    let temp = arr[i]
-    innerLoop(i, i, temp, () => {
-      setTimeout(() => {
-        outerLoop(i + 1, next)
-      }, dummyTimestep)
-    })
-  }
-
-  setTimeout(() => {
-    outerLoop(1, () => {
-      next(arr)
-    })
-  }, dummyTimestep)
-}
-
-module.exports = {
-  sync: insertionSort,
-  async: insertionSortAsync
-}
+module.exports = insertionSort
